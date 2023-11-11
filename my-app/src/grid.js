2a99ca21f5754fc35040eeafb101617d971b9238
Washeem2002 <jkrowling7702@gmail.com>
2023-11-11 15:53:28 +0530

    vercel

--- my-app/src/grid.js
+++ my-app/src/grid.js
@@ -11,7 +11,7 @@ const Comp=(props)=>{
   
     
     useEffect(()=>{
-         fetch(`${process.env.REACT_APP_BASE_URL}/post`,{
+         fetch("/post",{
             method:"POST",
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({name:name})
